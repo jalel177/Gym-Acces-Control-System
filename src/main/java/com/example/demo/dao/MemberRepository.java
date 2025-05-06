dfeff266129dde8885f664527ed9b7b49cd8ebb4
jalel177 <jalelbensassi774@gmail.com>
2025-05-06 20:31:47 +0200

    message

--- src/main/java/com/example/demo/dao/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.model.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member,Long> {
-}
